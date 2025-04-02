04e54c8bd38ce7037ba401eeac3506cfa0c5a2ce
Manuel Diaz <manueldiaz.prof@gmail.com>
2025-04-02 15:24:53 -0300

    Desarrollo de rutas estaticas y dinamicas (con parámetros)

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,15 +1,17 @@
+import { NavLink } from 'react-router'
 import './MainHeader.css'
 export const MainHeader = () => {
+
     return (
         <header className="main-header">
             <div className="container">
                 <h1 className="logo">Centro odontologico</h1>
                 <nav className="menu">
                     <ul className="listado-menu">
-                        <li className="item-menu"><a className="enlace-menu" href="#">Inicio</a></li>
-                        <li className="item-menu"><a className="enlace-menu" href="#">Servicios</a></li>
-                        <li className="item-menu"><a className="enlace-menu" href="#">Turnos</a></li>
-                        <li className="item-menu"><a className="enlace-menu" href="#">Profesionales</a></li>
+                        <li className="item-menu"><NavLink className="enlace-menu" to="/">Inicio</NavLink></li>
+                        <li className="item-menu"><NavLink className="enlace-menu" to="/servicios">Servicios</NavLink></li>
+                        <li className="item-menu"><NavLink className="enlace-menu" to="/turnos">Turnos</NavLink></li>
+                        <li className="item-menu"><NavLink className="enlace-menu" to="/contacto">Contacto</NavLink></li>
                     </ul>
                 </nav>
             </div>
--- src/pages/Servicios/CardServicio.jsx
+++ src/pages/Servicios/CardServicio.jsx
@@ -1,26 +1,16 @@
-import { useState } from "react";
-import { RightArrowIcon } from "../../Icons/Icons";
+
+import { RightArrowIcon } from "../../components/Icons/Icons";
 
 
-export const CardServicio = ({ titulo, descripcion, imagen, favorito, descontarContadorFav, aumentarContadorFav }) => {
+export const CardServicio = ({ titulo, descripcion, imagen}) => {
     /*
         A TRAVÉS DEL PARÁMETRO props -> NOS LLEGA UN OBJETO CON TODAS LAS PROPIEDADES QUE SE LE PASARON AL COMPONENTE CUANDO LO INVOCARON.
         COMO TODO OBJETO PODEMOS DESESTRUCTURARLO -> const {titulo, descripcion,imagen} = props;
     */
-    const [isFavorite, setIsFavorite] = useState(favorito);
-
-    const handleClickFavorito = () => {
-        isFavorite ? descontarContadorFav() : aumentarContadorFav();
-
-        setIsFavorite(!isFavorite);
-    }
 
 
     return (
         <article className="info-servicio">
-
-
-            <button onClick={handleClickFavorito}>{isFavorite ? 'Quitar de favoritos' : 'Agregar a favorito'}</button>
 
             <div className="contenedor-img-servicio">
                 <img className="img-servicio" src={`/${imagen}`} alt="" />
