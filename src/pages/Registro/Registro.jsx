832b020eae874b7a1c445b28ad008be70082bd7a
Matias <matias.ivanoff8@gmail.com>
2025-04-11 15:00:06 -0300

    Desarrollo de formulario de registro

--- src/pages/Registro/Registro.jsx
+++ src/pages/Registro/Registro.jsx
@@ -9,31 +9,58 @@ export const Registro = () => {
     const [confirmPassword, setConfirmPassword] = useState('')
     */
     const [formData, setFormData] = useState({nombre:'', email:'', password:'', confirmPassword:''})
+    const [formError, setFormError] = useState({nombre:'', email:'', password:'', confirmPassword:''})
+
+    const regexEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+    const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){6,15}$/
 
     function handleChangeInput(event) {
         setFormData({...formData, [event.target.name]:event.target.value})
     }
 
-    function handleChangeName(value) {
-        setNombre(value)
-    }
-
-    function handleChangeEmail(value) {
-        setEmail(value) 
-    }
-
-    function handleChangePassword(value) {
-        setPassword(value) 
-    }
-
-    function handleChangeConfirmPassword(value) {
-        setConfirmPassword(value) 
-    }
-
+    
     function handleSubmit(event) {
         event.preventDefault()
+        let errores = {};
 
-        console.log(formData);
+        const {nombre, email, password, confirmPassword} = formData
+
+        if (nombre.length < 4) {
+            //*console.error("Error! El nombre debe contener al menos 4 caracteres.")
+            errores = {...errores, nombre:'Error! El nombre debe contener al menos 4 caracteres.'}
+        }
+
+        if (email.length < 8 ) {
+            //*console.error("Error! El email debe contener al menos 8 caracteres.")
+            errores = {...errores, email:'Error! El email debe contener al menos 8 caracteres.'}
+
+        } else if (!regexEmail.test(email)){
+            //*console.error("Error! El email ingresado debe ser válido.")
+            errores = {...errores, email:'Error! Error! El email ingresado debe ser válido.'}
+        }
+
+        if (password.length < 6) {
+            //*console.error("Error! La constraseña debe contener al menos 6 caracteres.")
+            errores = {...errores, password:'Error! La constraseña debe contener al menos 6 caracteres.'}
+
+        } else if (!regexPassword.test(password)){
+            //*console.error("Error! La constraseña debe contener: mínimo 6 caracteres, maximo 15 caracteres; Al menos una letra mayúscula, Al menos una letra minúscula; Al menos un dígito; No espacio en blanco; Al menos un caracter especial")
+            errores = {...errores, password:'Error! La constraseña debe contener: mínimo 6 caracteres, maximo 15 caracteres; Al menos una letra mayúscula, Al menos una letra minúscula; Al menos un dígito; No espacio en blanco; Al menos un caracter especial.'}
+        }
+
+        if (password !== confirmPassword) {
+            //*console.error("Error! Las constraseñas no coinciden.")
+            errores = {...errores, confirmPassword:'Error! Las constraseñas no coinciden.'}
+        }
+
+        setFormError(errores)
+
+        if (Object.keys(errores).length == 0) {
+            console.log(formData);
+        } else {
+            console.error("Hay errores") 
+        }
+        
         
     }
     
@@ -52,9 +79,28 @@ export const Registro = () => {
             <form onSubmit={(event) => handleSubmit(event)}>
 
                 <input name='nombre' onChange={(event) => handleChangeInput(event)} type="text" placeholder='Ingrese su nombre'/>
+                {
+                    formError.nombre && 
+                    <p className='form-error'>{formError.nombre}</p>
+                }
+                
                 <input name='email' onChange={(event) => handleChangeInput(event)} type="email" placeholder='Ingrese su email'/>
+                {
+                    formError.email && 
+                    <p className='form-error'>{formError.email}</p>
+                }
+
                 <input name='password' onChange={(event) => handleChangeInput(event)} type="password" placeholder='Ingrese su contraseña'/>
+                {
+                    formError.password && 
+                    <p className='form-error'>{formError.password}</p>
+                }
+
                 <input name='confirmPassword' onChange={(event) => handleChangeInput(event)} type="password"  placeholder='Confirmar contraseña'/>
+                {
+                    formError.confirmPassword && 
+                    <p className='form-error'>{formError.confirmPassword}</p>
+                }
 
                 <button type="submit">Registrarme</button>
             </form>
